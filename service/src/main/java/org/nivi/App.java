973acdf5abb0f04d2191d34aee2dd8e1aaf552a3
Gayatri Ajith <gayatri@schogini.com>
2020-10-21 00:43:53 +0530

    Added read properties file section to the service project too

--- service/src/main/java/org/nivi/App.java
+++ service/src/main/java/org/nivi/App.java
@@ -1,5 +1,9 @@
 package org.nivi;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,25 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	
+	try {
+	    InputStream input = App.class.getClassLoader().getResourceAsStream("common.properties");
+            if (input == null) {
+                System.out.println("Sorry, unable to find common.properties");
+                return;
+            }
+
+            Properties prop = new Properties();
+
+            // load a properties file from class path, inside static method
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("message"));
+            System.out.println(prop.getProperty("additional"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
